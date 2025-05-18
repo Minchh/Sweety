83308fc6313da512407d7b6581e9f2cde92c3b8a
Minchh <minh.nguyendo3103@gmail.com>
2025-05-19 02:42:54 +0700

    Create a simple CRUD Product API

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,18 @@
 import express from "express";
+import bodyParser from "body-parser";
+
+import { connectDB } from "./config/index.js"
+import { productRouter } from "./api/routes/index.js";
 
 const app = express();
+
+// Middlewares
+app.use(bodyParser.json());
+
+// Routes
+app.use("/api/v1/products", productRouter);
+
+// MongoDB connection
+await connectDB();
 
 export default app;
