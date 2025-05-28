94b75118b080d095cf7cf672f4fce3acec190de6
btrannn <le.btrann@gmail.com>
2025-05-28 18:32:53 +0700

    Home Page: remove redundant lines of code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { createBrowserRouter, RouterProvider, useLocation } from "react-router";
 import "./css/App.css";
 
 import Home from "./pages/Home.jsx";
-import Products from "./pages/Products.jsx";
 
 const router = createBrowserRouter([
     {
